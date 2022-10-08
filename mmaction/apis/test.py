044802c01ac29b68937ad04524ad513cfb5d6a31
Kiet Nguyen <kanguyen.vn@gmail.com>
2022-10-07 22:42:03 -0400

    REMOVE: test debug

--- mmaction/apis/test.py
+++ mmaction/apis/test.py
@@ -2,6 +2,7 @@ import os.path as osp
 import pickle
 import shutil
 import tempfile
+
 # TODO import test functions from mmcv and delete them from mmaction2
 import warnings
 
@@ -11,14 +12,20 @@ import torch.distributed as dist
 from mmcv.runner import get_dist_info
 
 try:
-    from mmcv.engine import (single_gpu_test, multi_gpu_test,
-                             collect_results_gpu, collect_results_cpu)
+    from mmcv.engine import (
+        single_gpu_test,
+        multi_gpu_test,
+        collect_results_gpu,
+        collect_results_cpu,
+    )
+
     from_mmcv = True
 except (ImportError, ModuleNotFoundError):
     warnings.warn(
-        'DeprecationWarning: single_gpu_test, multi_gpu_test, '
-        'collect_results_cpu, collect_results_gpu from mmaction2 will be '
-        'deprecated. Please install mmcv through master branch.')
+        "DeprecationWarning: single_gpu_test, multi_gpu_test, "
+        "collect_results_cpu, collect_results_gpu from mmaction2 will be "
+        "deprecated. Please install mmcv through master branch."
+    )
     from_mmcv = False
 
 if not from_mmcv:
@@ -41,6 +48,7 @@ if not from_mmcv:
         dataset = data_loader.dataset
         prog_bar = mmcv.ProgressBar(len(dataset))
         for data in data_loader:
+            print(data.shape)
             with torch.no_grad():
                 result = model(return_loss=False, **data)
             results.extend(result)
@@ -51,8 +59,7 @@ if not from_mmcv:
                 prog_bar.update()
         return results
 
-    def multi_gpu_test(  # noqa: F811
-            model, data_loader, tmpdir=None, gpu_collect=True):
+    def multi_gpu_test(model, data_loader, tmpdir=None, gpu_collect=True):  # noqa: F811
         """Test model with multiple gpus.
 
         This method tests model with multiple gpus and collects the results
@@ -116,18 +123,14 @@ if not from_mmcv:
         if tmpdir is None:
             MAX_LEN = 512
             # 32 is whitespace
-            dir_tensor = torch.full((MAX_LEN, ),
-                                    32,
-                                    dtype=torch.uint8,
-                                    device='cuda')
+            dir_tensor = torch.full((MAX_LEN,), 32, dtype=torch.uint8, device="cuda")
             if rank == 0:
-                mmcv.mkdir_or_exist('.dist_test')
-                tmpdir = tempfile.mkdtemp(dir='.dist_test')
+                mmcv.mkdir_or_exist(".dist_test")
+                tmpdir = tempfile.mkdtemp(dir=".dist_test")
                 tmpdir = torch.tensor(
-                    bytearray(tmpdir.encode()),
-                    dtype=torch.uint8,
-                    device='cuda')
-                dir_tensor[:len(tmpdir)] = tmpdir
+                    bytearray(tmpdir.encode()), dtype=torch.uint8, device="cuda"
+                )
+                dir_tensor[: len(tmpdir)] = tmpdir
             dist.broadcast(dir_tensor, 0)
             tmpdir = dir_tensor.cpu().numpy().tobytes().decode().rstrip()
         else:
@@ -135,7 +138,7 @@ if not from_mmcv:
         # synchronizes all processes to make sure tmpdir exist
         dist.barrier()
         # dump the part result to the dir
-        mmcv.dump(result_part, osp.join(tmpdir, f'part_{rank}.pkl'))
+        mmcv.dump(result_part, osp.join(tmpdir, f"part_{rank}.pkl"))
         # synchronizes all processes for loding pickle file
         dist.barrier()
         # collect all parts
@@ -144,7 +147,7 @@ if not from_mmcv:
         # load results of all parts from tmp dir
         part_list = []
         for i in range(world_size):
-            part_file = osp.join(tmpdir, f'part_{i}.pkl')
+            part_file = osp.join(tmpdir, f"part_{i}.pkl")
             part_list.append(mmcv.load(part_file))
         # sort the results
         ordered_results = []
@@ -172,28 +175,24 @@ if not from_mmcv:
         rank, world_size = get_dist_info()
         # dump result part to tensor with pickle
         part_tensor = torch.tensor(
-            bytearray(pickle.dumps(result_part)),
-            dtype=torch.uint8,
-            device='cuda')
+            bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device="cuda"
+        )
         # gather all result part tensor shape
-        shape_tensor = torch.tensor(part_tensor.shape, device='cuda')
+        shape_tensor = torch.tensor(part_tensor.shape, device="cuda")
         shape_list = [shape_tensor.clone() for _ in range(world_size)]
         dist.all_gather(shape_list, shape_tensor)
         # padding result part tensor to max length
         shape_max = torch.tensor(shape_list).max()
-        part_send = torch.zeros(shape_max, dtype=torch.uint8, device='cuda')
-        part_send[:shape_tensor[0]] = part_tensor
-        part_recv_list = [
-            part_tensor.new_zeros(shape_max) for _ in range(world_size)
-        ]
+        part_send = torch.zeros(shape_max, dtype=torch.uint8, device="cuda")
+        part_send[: shape_tensor[0]] = part_tensor
+        part_recv_list = [part_tensor.new_zeros(shape_max) for _ in range(world_size)]
         # gather all result part
         dist.all_gather(part_recv_list, part_send)
 
         if rank == 0:
             part_list = []
             for recv, shape in zip(part_recv_list, shape_list):
-                part_list.append(
-                    pickle.loads(recv[:shape[0]].cpu().numpy().tobytes()))
+                part_list.append(pickle.loads(recv[: shape[0]].cpu().numpy().tobytes()))
             # sort the results
             ordered_results = []
             for res in zip(*part_list):
